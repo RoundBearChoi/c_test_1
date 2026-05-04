1dba44c95751334fd81fc6b1713a28456c1d4a37
roundbearchoi <roundbeargames@gmail.com>
2026-05-04 12:39:43 +0900

    save

--- src/example.c
+++ src/example.c
@@ -1,6 +1,5 @@
 #include "example.h"
 
-// Function definition
 int add(int a, int b)
 {
     return a + b;
--- src/example.h
+++ src/example.h
@@ -1,7 +1,6 @@
 #ifndef example_h
 #define example_h
 
-// Function declaration (prototype)
 int add(int a, int b);
 
-#endif // ADD_H
+#endif
